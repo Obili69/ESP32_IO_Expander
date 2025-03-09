e740caa682fb843dc98f9cf7aee26de7e652eb66
Obili69 <christian.magnus@obrecht.ch>
2025-03-09 17:05:36 +0100

    wh

---
+++ src/ESP_IOExpander_Library.h
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ESP_IOEXPANDER_LIBRARY_H
+#define ESP_IOEXPANDER_LIBRARY_H
+
+#include "ESP_IOExpander.h"
+
+#include "chip/TCA95xx_8bit.h"
+
+#endif
